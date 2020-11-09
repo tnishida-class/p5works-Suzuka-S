6dbb203b77f21e0956df87a33c94aa8dc04d4ad3
Suzuka-S <65203146+Suzuka-S@users.noreply.github.com>
2020-11-09 20:28:44 +0900

    Update section3-1
    EUの国旗になるように書き換えました。

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -1,12 +1,24 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
-  background(192);
+  createCanvas(300, 200);
+  background(0, 54, 217);
+  noStroke();
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    let x = 150 + cos(theta) * 70;
+    let y = 100 + sin(theta) * 70;
+    star(x, y, 10);
   }
+}
+function star(cx, cy, r){
+  fill(252, 202, 3);
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
 }
