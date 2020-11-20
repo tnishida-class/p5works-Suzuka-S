b66423feaa1d87a6a686692056f7b40f87726fba
Suzuka-S <65203146+Suzuka-S@users.noreply.github.com>
2020-11-20 15:22:00 +0900

    Update section4-q1
    円が心臓の動きのように見えるように書き換えました。

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,29 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
+
+let tx1 = width / 3 + 50;
+// let tx2;
+// let tx3;
+// let ty1;
+// let ty2;
+// let ty3;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   count = 0;
   cycle = 100;
+  size = 50;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  if(count < 40){size += 2;}
+  else if(count > 40 && count < 50){size++;}
+  else {size--;}
+  size = constrain(size, 50, 101);
   ellipse(width / 2, height / 2, size);
 }
