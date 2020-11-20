b734f5877f9cecb4c1559d68a0c7621e2ab2c437
Suzuka-S <65203146+Suzuka-S@users.noreply.github.com>
2020-11-20 16:11:54 +0900

    Update section4-q2
    星が回転しながら画面上を流れるように書き換えました。

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,5 +1,5 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -7,11 +7,17 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+  star(x, y, 30, angle);
+  x -= vx;
+  y += vy;
+  angle += TWO_PI / 50;
+
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -25,8 +31,8 @@ function star(cx, cy, r, angle){
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
-    var x = cx + cos(theta) * r;
-    var y = cy + sin(theta) * r;
+    var x = cx + cos(theta + angle) * r;
+    var y = cy + sin(theta + angle) * r;
     vertex(x,y);
   }
   endShape(CLOSE);
